f5eb943435f71507b9e36f7533e84540b379bc95
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-04-17 10:35:22 -0700

    successfully scrape single lesson page text

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -3,7 +3,12 @@ class ContentController < ApplicationController
     require 'openssl'
     require 'open-uri'
     doc = Nokogiri::HTML(open('https://www.learnhowtoprogram.com/rails/ruby-on-rails-basics/database-scopes-and-queries', :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
-    lesson_test = doc.css('.tab-pane')
+    @lesson_paragraphs = []
+    @lesson_codes = []
+    @lesson_titles = []
+    doc.css('.tab-pane').children.map do |x|
+      @lesson_paragraphs << x.text
+    end
     binding.pry
     render :lesson
   end
